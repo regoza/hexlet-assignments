b17c05f07904d114d529a539eaea9c0fbe80b8be
dmitriy <dimons12@gmail.com>
2023-03-14 22:16:36 +0300

    Реализуйте сервлет, который принимает GET запрос и в ответ отправляет строку 'Hello, Hexlet!'

---
+++ java-web-ru/servlet/src/main/java/exercise/servlet/WelcomeServlet.java
@@ -0,0 +1,20 @@
+package exercise.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// BEGIN
+
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello, Hexlet!");
+    }
+}
+
+// END
